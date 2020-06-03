ac9a73e6b42b9c4cbcdcfc89a5eec1c5cece6a96
Georgette Class-Peters <georgycp@udel.edu>
2020-06-03 21:53:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,12 +2,12 @@ require 'pry'
 
 def oxford_comma(array)
 if array.length == 1
-   array[0]
- elsif array.length == 2
-   array[0] + " and "+ array[1]
+  array[0]
+elsif array.length == 2
+  array[0] + " and " + array[1]
  else
 #binding.pry
   array[-1] = "and " + arrary[-1]
   array.join(", ")
-  end 
+end 
 end
